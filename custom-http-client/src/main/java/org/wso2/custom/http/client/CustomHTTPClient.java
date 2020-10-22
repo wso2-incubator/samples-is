704b80163089470595c2577a11a659fc0d3cf238
Tharindu Bandara <tharindu.bandara.edu@gmail.com>
2020-10-23 02:13:40 +0530

    Update licence header

--- custom-http-client/src/main/java/org/wso2/custom/http/client/CustomHTTPClient.java
+++ custom-http-client/src/main/java/org/wso2/custom/http/client/CustomHTTPClient.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.custom.http.client;
 
 import org.wso2.custom.http.client.config.CustomHTTPClientBaseConfiguration;
@@ -102,7 +120,6 @@ public class CustomHTTPClient {
 
             String authHeader = getAuthenticationHeader(true);
             request.setHeader("Authorization", "Basic " + authHeader);
-            request.setHeader("X-Company-Code",  CustomHTTPClientBaseConfiguration.getPropertyCode());
 
             if (log.isDebugEnabled()) {
                 log.debug("Invocation of Async API to " + epUrl + ", with the payload : " + payload);
@@ -200,8 +217,6 @@ public class CustomHTTPClient {
 
             String authHeader = getAuthenticationHeader(true);
             request.setHeader("Authorization", authHeader);
-            request.setHeader("X-Company-Code",  CustomHTTPClientBaseConfiguration.getPropertyCode());
-            request.setHeader("X-P360-Validation",  "true");
 
             CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
 
--- custom-http-client/src/main/java/org/wso2/custom/http/client/config/CustomHTTPClientBaseConfiguration.java
+++ custom-http-client/src/main/java/org/wso2/custom/http/client/config/CustomHTTPClientBaseConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.custom.http.client.config;
 
 import org.apache.commons.logging.Log;
@@ -16,7 +34,6 @@ public class CustomHTTPClientBaseConfiguration implements CustomHTTPClientConfig
     private static final String CONFIG_FILE = "config.properties";
     private static final String CONFIG_FILE_PATH = CarbonUtils.getCarbonConfigDirPath() + File.separator + CONFIG_FILE;
 
-    private static final String PROPERTY_CODE_KEY = "property.code";
     private static final String CONNECTION_API_KEY = "connection.api.key";
     private static final String CONNECTION_TIMEOUT_KEY = "connection.timeout";
     private static final String CONNECTION_REQUEST_TIMEOUT_KEY = "connection.request.timeout";
@@ -45,12 +62,6 @@ public class CustomHTTPClientBaseConfiguration implements CustomHTTPClientConfig
 
         return properties;
     }
-
-    public static String getPropertyCode() {
-
-        return properties.getProperty(PROPERTY_CODE_KEY);
-    }
-
 
     public static String getAPIKey() {
 
